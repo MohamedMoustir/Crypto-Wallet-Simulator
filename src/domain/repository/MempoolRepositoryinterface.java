8f418a26d45a4d7125073229c401053e335dcffa
MohamedMoustir <itsmoustir@gmail.com>
2025-09-29 21:18:10 +0100

    Add MempoolRepositoryinterface.java to manage pending transactions

--- src/domain/repository/MempoolRepositoryinterface.java
+++ src/domain/repository/MempoolRepositoryinterface.java
@@ -1,14 +1,16 @@
 package domain.repository;
 
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+
 import domain.model.Mempool;
 import domain.model.Transaction;
-
-import java.time.LocalDateTime;
-import java.util.PriorityQueue;
 
 public interface MempoolRepositoryinterface {
 
 	boolean CreateMemPool(Transaction transaction, int position_in_queue, double fee, double estimatedTime);
 
 	PriorityQueue<Mempool> GetAllMemepool();
+	List<Map<String, Object>> getMemPool();
 }
